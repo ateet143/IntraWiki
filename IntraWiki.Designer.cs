8f2941968c2d57f04871d56c78a34ddb0b018f08
ateet singh <ateet143@gmail.com>
2022-02-16 23:46:13 +0800

    Sorted the array according to the column 0

--- IntraWiki.Designer.cs
+++ IntraWiki.Designer.cs
@@ -46,9 +46,10 @@
             // 
             this.buttonAdd.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.buttonAdd.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.buttonAdd.Location = new System.Drawing.Point(49, 45);
+            this.buttonAdd.Location = new System.Drawing.Point(74, 69);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonAdd.Name = "buttonAdd";
-            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.Size = new System.Drawing.Size(112, 35);
             this.buttonAdd.TabIndex = 0;
             this.buttonAdd.Text = "ADD";
             this.buttonAdd.UseVisualStyleBackColor = false;
@@ -56,9 +57,10 @@
             // buttonSearch
             // 
             this.buttonSearch.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.buttonSearch.Location = new System.Drawing.Point(544, 43);
+            this.buttonSearch.Location = new System.Drawing.Point(816, 66);
+            this.buttonSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonSearch.Name = "buttonSearch";
-            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.Size = new System.Drawing.Size(112, 35);
             this.buttonSearch.TabIndex = 1;
             this.buttonSearch.Text = "SEARCH";
             this.buttonSearch.UseVisualStyleBackColor = false;
@@ -66,9 +68,10 @@
             // buttonOpen
             // 
             this.buttonOpen.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.buttonOpen.Location = new System.Drawing.Point(468, 388);
+            this.buttonOpen.Location = new System.Drawing.Point(702, 597);
+            this.buttonOpen.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonOpen.Name = "buttonOpen";
-            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.Size = new System.Drawing.Size(112, 35);
             this.buttonOpen.TabIndex = 2;
             this.buttonOpen.Text = "OPEN";
             this.buttonOpen.UseVisualStyleBackColor = false;
@@ -76,9 +79,10 @@
             // buttonSave
             // 
             this.buttonSave.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.buttonSave.Location = new System.Drawing.Point(577, 388);
+            this.buttonSave.Location = new System.Drawing.Point(866, 597);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.Size = new System.Drawing.Size(112, 35);
             this.buttonSave.TabIndex = 3;
             this.buttonSave.Text = "SAVE";
             this.buttonSave.UseVisualStyleBackColor = false;
@@ -87,55 +91,63 @@
             // 
             this.listBoxWki.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBoxWki.FormattingEnabled = true;
-            this.listBoxWki.ItemHeight = 18;
-            this.listBoxWki.Location = new System.Drawing.Point(394, 100);
+            this.listBoxWki.ItemHeight = 25;
+            this.listBoxWki.Location = new System.Drawing.Point(591, 154);
+            this.listBoxWki.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBoxWki.Name = "listBoxWki";
-            this.listBoxWki.Size = new System.Drawing.Size(338, 274);
+            this.listBoxWki.Size = new System.Drawing.Size(505, 404);
             this.listBoxWki.TabIndex = 4;
+            this.listBoxWki.SelectedIndexChanged += new System.EventHandler(this.listBoxWki_SelectedIndexChanged);
             // 
             // textBoxSearch
             // 
-            this.textBoxSearch.Location = new System.Drawing.Point(385, 45);
+            this.textBoxSearch.Location = new System.Drawing.Point(578, 69);
+            this.textBoxSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxSearch.Name = "textBoxSearch";
-            this.textBoxSearch.Size = new System.Drawing.Size(127, 20);
+            this.textBoxSearch.Size = new System.Drawing.Size(188, 26);
             this.textBoxSearch.TabIndex = 5;
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(49, 113);
+            this.textBoxName.Location = new System.Drawing.Point(74, 174);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(127, 20);
+            this.textBoxName.Size = new System.Drawing.Size(188, 26);
             this.textBoxName.TabIndex = 6;
             // 
             // textBoxCategory
             // 
-            this.textBoxCategory.Location = new System.Drawing.Point(49, 183);
+            this.textBoxCategory.Location = new System.Drawing.Point(74, 282);
+            this.textBoxCategory.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxCategory.Name = "textBoxCategory";
-            this.textBoxCategory.Size = new System.Drawing.Size(127, 20);
+            this.textBoxCategory.Size = new System.Drawing.Size(188, 26);
             this.textBoxCategory.TabIndex = 7;
             // 
             // textBoxStructure
             // 
-            this.textBoxStructure.Location = new System.Drawing.Point(49, 249);
+            this.textBoxStructure.Location = new System.Drawing.Point(74, 383);
+            this.textBoxStructure.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxStructure.Name = "textBoxStructure";
-            this.textBoxStructure.Size = new System.Drawing.Size(127, 20);
+            this.textBoxStructure.Size = new System.Drawing.Size(188, 26);
             this.textBoxStructure.TabIndex = 8;
             // 
             // textBoxDefinition
             // 
-            this.textBoxDefinition.Location = new System.Drawing.Point(49, 311);
+            this.textBoxDefinition.Location = new System.Drawing.Point(74, 478);
+            this.textBoxDefinition.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxDefinition.Multiline = true;
             this.textBoxDefinition.Name = "textBoxDefinition";
-            this.textBoxDefinition.Size = new System.Drawing.Size(127, 120);
+            this.textBoxDefinition.Size = new System.Drawing.Size(484, 182);
             this.textBoxDefinition.TabIndex = 9;
             // 
             // buttonEdit
             // 
             this.buttonEdit.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.buttonEdit.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.buttonEdit.Location = new System.Drawing.Point(240, 43);
+            this.buttonEdit.Location = new System.Drawing.Point(360, 66);
+            this.buttonEdit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonEdit.Name = "buttonEdit";
-            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.Size = new System.Drawing.Size(112, 35);
             this.buttonEdit.TabIndex = 10;
             this.buttonEdit.Text = "EDIT";
             this.buttonEdit.UseVisualStyleBackColor = false;
@@ -144,18 +156,19 @@
             // 
             this.buttonDelete.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.buttonDelete.ForeColor = System.Drawing.Color.Transparent;
-            this.buttonDelete.Location = new System.Drawing.Point(239, 111);
+            this.buttonDelete.Location = new System.Drawing.Point(358, 171);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonDelete.Name = "buttonDelete";
-            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.Size = new System.Drawing.Size(112, 35);
             this.buttonDelete.TabIndex = 11;
             this.buttonDelete.Text = "DELETE";
             this.buttonDelete.UseVisualStyleBackColor = false;
             // 
             // IntraWiki
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.buttonDelete);
             this.Controls.Add(this.buttonEdit);
             this.Controls.Add(this.textBoxDefinition);
@@ -169,6 +182,7 @@
             this.Controls.Add(this.buttonSearch);
             this.Controls.Add(this.buttonAdd);
             this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "IntraWiki";
             this.Text = "IntraWiki";
             this.Load += new System.EventHandler(this.IntraWiki_Load);
--- IntraWiki.cs
+++ IntraWiki.cs
@@ -21,16 +21,16 @@ namespace IntraWiki
         static int colSize = 5;
         static string[,] myArray = new string[rowSize, colSize];
 
-       
+
 
         private void DisplayArray()
         {
             listBoxWki.Items.Clear();
-          /*  for(int i=0; i< 1; i++)
-            {
-                listBoxWki.Items.Add("List of items");
-                listBoxWki.Items.Add("------------------------");
-            }*/
+            /*  for(int i=0; i< 1; i++)
+              {
+                  listBoxWki.Items.Add("List of items");
+                  listBoxWki.Items.Add("------------------------");
+              }*/
             for (int x = 0; x < rowSize; x++)
             {
                 string oneLine = "";
@@ -39,25 +39,29 @@ namespace IntraWiki
             }
         }
 
-        private void populateTitle()
+        private void sortName()
         {
-            myArray[0, 0] = "Array";
-            myArray[1, 0] = "Two Dimension Array";
-            myArray[2, 0] = "List";
-            myArray[3, 0] = "Linked list";
-            myArray[4, 0] = "Self - Balance Tree";
-            myArray[5, 0] = "Heaps";
-            myArray[6, 0] = "Binary Search Tree";
-            myArray[7, 0] = "Graph";
-            myArray[8, 0] = "Set";
-            myArray[9, 0] = "Queue";
-            myArray[10, 0] = "Stack";
-            myArray[11, 0] = "Hash Table";
-            for(int i=0; i< rowSize; i++)
+            int num = 0;
+            for (int rowX = 0; rowX < rowSize; rowX++)
             {
-                myArray[0, i] = myArray[i, 0];
-            }
+                for (int j = rowX + 1; j < rowSize; j++)
+                {
 
+                    if ((myArray[rowX, num].CompareTo(myArray[j, num]) > 0))
+                    {
+                        string temp = myArray[rowX, num];
+                        myArray[rowX, num] = myArray[j, num];
+                        myArray[j, num] = temp;
+
+                        for (int k = 1; k < colSize; k++)
+                        {
+                            string temp1 = myArray[rowX, k];
+                            myArray[rowX, k] = myArray[j, k];
+                            myArray[j, k] = temp1;
+                        }
+                    }
+                }
+            }
         }
 
         private void IntraWiki_Load(object sender, EventArgs e)
@@ -74,7 +78,61 @@ namespace IntraWiki
             myArray[9, 0] = "Queue";
             myArray[10, 0] = "Stack";
             myArray[11, 0] = "Hash Table";
+
+            myArray[0, 1] = "Array"; myArray[0, 2] = "Linear";
+            myArray[1, 1] = "Array"; myArray[1, 2] = "Linear";
+            myArray[2, 1] = "List"; myArray[2, 2] = "Linear";
+            myArray[3, 1] = "List"; myArray[3, 2] = "Linear";
+            myArray[4, 1] = "Tree"; myArray[4, 2] = "Non-Linear";
+            myArray[5, 1] = "Tree"; myArray[5, 2] = "Non-Linear";
+            myArray[6, 1] = "Tree"; myArray[6, 2] = "Non-Linear";
+            myArray[7, 1] = "Graphs"; myArray[7, 2] = "Non-Linear";
+            myArray[8, 1] = "Abstract"; myArray[8, 2] = "Non-Linear";
+            myArray[9, 1] = "Abstract"; myArray[9, 2] = "Linear";
+            myArray[10, 1] = "Abstract"; myArray[10, 2] = "Linear";
+            myArray[11, 1] = "Hash"; myArray[11, 2] = "Non-Linear";
+
+            myArray[0, 3] = "An array is a collection of elements of the same type placed in contiguous memory locations that can be individually referenced by using an index to a unique identifier.";
+            myArray[1, 3] = "Arrays can have more than one dimension.The multidimensional array can be declared by adding commas in the square brackets. For example, [,] declares two-dimensional array";
+            myArray[2, 3] = "The List<T> is a collection of strongly typed objects that can be accessed by index and having methods for sorting, searching, and modifying list. It is the generic version of the ArrayList that comes under System.Collection.Generic namespace.";
+            myArray[3, 3] = "A LinkedList is a linear data structure which stores element in the non-contiguous location. The elements in a linked list are linked with each other using pointers. Or in other words, LinkedList consists of nodes where each node contains a data field and a reference(link) to the next node in the list.";
+            myArray[4, 3] = " A self-balancing binary search tree (BST) is any node-based binary search tree that automatically keeps its height (maximal number of levels below the root) small in the face of arbitrary item insertions and deletions.";
+            myArray[5, 3] = "A Heap is a special Tree-based data structure in which the tree is a complete binary tree. Generally, Heaps can be of two types: Max-Heap: In a Max-Heap the key present at the root node must be greatest among the keys present at all of it's children.";
+            myArray[6, 3] = "A binary search tree (BST) is a binary tree where each node has a Comparable key (and an associated value) and satisfies the restriction that the key in any node is larger than the keys in all nodes in that node's left subtree and smaller than the keys in all nodes in that node's right subtree.";
+            myArray[7, 3] = "A Graph is a non-linear data structure consisting of nodes and edges. The nodes are sometimes also referred to as vertices and the edges are lines or arcs that connect any two nodes in the graph";
+            myArray[8, 3] = "A Graph is a non-linear data structure consisting of nodes and edges. The nodes are sometimes also referred to as vertices and the edges are lines or arcs that connect any two nodes in the graph";
+            myArray[9, 3] = "Queue is a special type of collection that stores the elements in FIFO style (First In First Out), exactly opposite of the Stack<T> collection. It contains the elements in the order they were added. C# includes generic Queue<T> and non-generic Queue collection. It is recommended to use the generic Queue<T> collection.";
+            myArray[10, 3] = "Stack is a special type of collection that stores elements in LIFO style (Last In First Out). C# includes the generic Stack<T> and non-generic Stack collection classes. It is recommended to use the generic Stack<T> collection.Stack is useful to store temporary data in LIFO style, and you might want to delete an element after retrieving its value.";
+            myArray[11, 3] = "Stack is a special type of collection that stores elements in LIFO style (Last In First Out). C# includes the generic Stack<T> and non-generic Stack collection classes. It is recommended to use the generic Stack<T> collection.Stack is useful to store temporary data in LIFO style, and you might want to delete an element after retrieving its value.";
+
+            sortName();
             DisplayArray();
+        }
+
+
+        public int findIndex(string a)
+        {
+            for (int i = 0; i < rowSize; i++)
+            {
+                if (myArray[i, 0].Equals(a))
+                {
+                    return i;
+                }
+
+            }
+            return -1;
+        }
+
+        private void listBoxWki_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string a = listBoxWki.SelectedItem.ToString().Trim();
+            int b = findIndex(a);
+            textBoxName.Text = myArray[b, 0];
+            textBoxCategory.Text = myArray[b, 1];
+            textBoxStructure.Text = myArray[b, 2];
+            textBoxDefinition.Text = myArray[b, 3];
+
+
         }
     }
 }
