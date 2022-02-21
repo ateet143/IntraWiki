86ce09ae3e49ae8ca100e7cb73079ea2ac7f5af1
ateet singh <ateet143@gmail.com>
2022-02-21 19:19:17 +0800

    Add function and file handling

--- IntraWiki.Designer.cs
+++ IntraWiki.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.buttonAdd = new System.Windows.Forms.Button();
             this.buttonSearch = new System.Windows.Forms.Button();
             this.buttonOpen = new System.Windows.Forms.Button();
@@ -40,6 +41,12 @@
             this.textBoxDefinition = new System.Windows.Forms.TextBox();
             this.buttonEdit = new System.Windows.Forms.Button();
             this.buttonDelete = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.buttonAutoLoadData = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonAdd
@@ -53,6 +60,7 @@
             this.buttonAdd.TabIndex = 0;
             this.buttonAdd.Text = "ADD";
             this.buttonAdd.UseVisualStyleBackColor = false;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
             // 
             // buttonSearch
             // 
@@ -75,6 +83,7 @@
             this.buttonOpen.TabIndex = 2;
             this.buttonOpen.Text = "OPEN";
             this.buttonOpen.UseVisualStyleBackColor = false;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
             // 
             // buttonSave
             // 
@@ -86,6 +95,7 @@
             this.buttonSave.TabIndex = 3;
             this.buttonSave.Text = "SAVE";
             this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
             // listBoxWki
             // 
@@ -114,6 +124,7 @@
             this.textBoxName.Name = "textBoxName";
             this.textBoxName.Size = new System.Drawing.Size(188, 26);
             this.textBoxName.TabIndex = 6;
+            this.textBoxName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxName_KeyPress);
             // 
             // textBoxCategory
             // 
@@ -122,6 +133,7 @@
             this.textBoxCategory.Name = "textBoxCategory";
             this.textBoxCategory.Size = new System.Drawing.Size(188, 26);
             this.textBoxCategory.TabIndex = 7;
+            this.textBoxCategory.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxCategory_KeyPress);
             // 
             // textBoxStructure
             // 
@@ -130,6 +142,7 @@
             this.textBoxStructure.Name = "textBoxStructure";
             this.textBoxStructure.Size = new System.Drawing.Size(188, 26);
             this.textBoxStructure.TabIndex = 8;
+            this.textBoxStructure.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxStructure_KeyPress);
             // 
             // textBoxDefinition
             // 
@@ -139,6 +152,7 @@
             this.textBoxDefinition.Name = "textBoxDefinition";
             this.textBoxDefinition.Size = new System.Drawing.Size(484, 182);
             this.textBoxDefinition.TabIndex = 9;
+            this.textBoxDefinition.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxDefinition_KeyPress);
             // 
             // buttonEdit
             // 
@@ -164,11 +178,48 @@
             this.buttonDelete.Text = "DELETE";
             this.buttonDelete.UseVisualStyleBackColor = false;
             // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 755);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1322, 32);
+            this.statusStrip1.TabIndex = 12;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.toolStripStatusLabel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(194, 25);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // buttonAutoLoadData
+            // 
+            this.buttonAutoLoadData.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.buttonAutoLoadData.Location = new System.Drawing.Point(1143, 66);
+            this.buttonAutoLoadData.Name = "buttonAutoLoadData";
+            this.buttonAutoLoadData.Size = new System.Drawing.Size(125, 51);
+            this.buttonAutoLoadData.TabIndex = 13;
+            this.buttonAutoLoadData.Text = "Auto Load Data";
+            this.buttonAutoLoadData.UseVisualStyleBackColor = true;
+            this.buttonAutoLoadData.Click += new System.EventHandler(this.buttonAutoLoadData_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorProvider1.ContainerControl = this;
+            // 
             // IntraWiki
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.ClientSize = new System.Drawing.Size(1322, 787);
+            this.Controls.Add(this.buttonAutoLoadData);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.buttonDelete);
             this.Controls.Add(this.buttonEdit);
             this.Controls.Add(this.textBoxDefinition);
@@ -181,11 +232,14 @@
             this.Controls.Add(this.buttonOpen);
             this.Controls.Add(this.buttonSearch);
             this.Controls.Add(this.buttonAdd);
-            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "IntraWiki";
             this.Text = "IntraWiki";
             this.Load += new System.EventHandler(this.IntraWiki_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -205,6 +259,10 @@
         private System.Windows.Forms.TextBox textBoxDefinition;
         private System.Windows.Forms.Button buttonEdit;
         private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button buttonAutoLoadData;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
 
