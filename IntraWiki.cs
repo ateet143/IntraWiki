5bcbc90f80bbc9130887d85560df217c964c3032
ateet singh <ateet143@gmail.com>
2022-03-13 23:21:02 +0800

    Comment added

--- IntraWiki.cs
+++ IntraWiki.cs
@@ -12,6 +12,10 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.Diagnostics;
 using System.Runtime.Serialization;
 
+/// Created by Atit
+/// Version 3
+/// This application is a prototype of the wikipedia, application has used 2 dimension array to store the 12 Data Structure information,However user can able to ADD, EDIT and DELETE the information in the application.
+/// Similarly, application open and saves file in the home folder, additionally user can able to sort and search the data by entering data in the textbox.
 
 namespace IntraWiki
 {
@@ -23,6 +27,7 @@ namespace IntraWiki
             InitializeComponent();
         }
 
+        // 8.1	Create a global 2D string array, use static variables for the dimensions (row, column),
         static int rowSize = 12;
         static int colSize = 4;
         static string[,] myArray = new string[rowSize, colSize];
@@ -33,6 +38,8 @@ namespace IntraWiki
         #endregion
 
         #region DISPLAY,APPLICATION AUTO LOAD, DATA AUTO LOAD
+
+      //  8.6	Create a display method that will show the following information in a List box: Name and Category,
         private void DisplayArray()
         {
             listBoxWiki.Items.Clear();
@@ -40,6 +47,7 @@ namespace IntraWiki
 
             for (int x = 0; x < rowSize; x++)
             {
+                //if the array has null or empty then it will not further process and list display. Helpful to optimise and prevent empty string in listbox
                 if (string.IsNullOrEmpty(myArray[x, 0]))
                 {
                     return;
@@ -50,6 +58,7 @@ namespace IntraWiki
             }
         }
 
+        //8.9	Create a LOAD button that will read the information from a binary file called definitions.dat into the 2D array,
         private void IntraWiki_Load(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialogVG = new OpenFileDialog();
@@ -97,6 +106,7 @@ namespace IntraWiki
 
         #region LISTBOX DISPLAY 
 
+      //  8.7	Create a method so the user can select a definition(Name) from the Listbox and all the information is displayed in the appropriate Textboxes,
         private void listBoxWki_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (listBoxWiki.SelectedIndex == -1 && listBoxCategory.SelectedIndex == -1)
@@ -119,11 +129,13 @@ namespace IntraWiki
 
         private void listBoxCategory_SelectedIndexChanged(object sender, EventArgs e)
         {
+            //if nothing is selected from Name and Category then execute this 
             if (listBoxWiki.SelectedIndex == -1 && listBoxCategory.SelectedIndex == -1)
             {
                 toolStripStatusLabel1.Text = "!Element in the List Not selected";
                 return;
             }
+            //when anything selected from Name listbox, it will show in the corresponding textbox.
             if (listBoxCategory.SelectedIndex != -1)
             {
                 listBoxWiki.ClearSelected();
@@ -139,6 +151,8 @@ namespace IntraWiki
         #endregion
 
         #region FILE HANDLING
+
+      //  8.8	Create a SAVE button so the information from the 2D array can be written into a binary file called definitions.dat which is sorted by Name,
         private void buttonOpen_Click(object sender, EventArgs e)
         {
             OpenFileDialog OpenBinary = new OpenFileDialog();
@@ -238,14 +252,18 @@ namespace IntraWiki
         #endregion
 
         #region ADDFUNCTIONS
+
+        //8.2	Create an ADD button that will store the information from the 4 text boxes into the 2D array,
         private void buttonAdd_Click(object sender, EventArgs e)
         {
-
+            // only add when all the textbox is not null or empty
             if (!string.IsNullOrEmpty(textBoxName.Text) && !string.IsNullOrEmpty(textBoxCategory.Text) && !string.IsNullOrEmpty(textBoxStructure.Text) && !string.IsNullOrEmpty(textBoxDefinition.Text))
             {
 
                 for (int x = 0; x < rowSize; x++)
                 {
+                    //if row is 11 and either of them is not empty and not - (which means there is data) it will return and display the message.
+                    // need - as we will replacing the deleted item with - (logic that user can also fill the deleted item with text)
                     if (x == 11 && (myArray[x, 0] != String.Empty && myArray[x, 0] != "-"))
                     {
                         MessageBox.Show("CANNOT EXCEED MORE THAN 12 ROWS", "*User Information*", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -253,6 +271,7 @@ namespace IntraWiki
                         return;
                     }
 
+                    // if the element has empty and - then it will add from textbox
                     if (myArray[x, 0] == String.Empty || myArray[x, 0] == "-")
                     {
                         myArray[x, 0] = textBoxName.Text;
@@ -266,6 +285,7 @@ namespace IntraWiki
                             toolStripStatusLabel1.Text = String.Format("\"{0}\" is Added to Row {1}... ", textBoxName.Text, x + 1);
                             break;
                         }
+                        // if user cancel to add, then fill the every element with empty string, to prevent the null value.
                         else
                         {
                             myArray[x, 0] = String.Empty;
@@ -283,6 +303,7 @@ namespace IntraWiki
             }
             else
             {
+                // will set the error message, inform the user the data is required or mandatory need to fill the textbox.
                 if (string.IsNullOrEmpty(textBoxName.Text))
                 {
                     errorProvider1.SetError(textBoxName, "Data Required...");
@@ -321,6 +342,7 @@ namespace IntraWiki
             DialogResult modifyTask = MessageBox.Show("Data will Permanently Deleted,Do you want to Continue?", "Delete the data...", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
             if (modifyTask == DialogResult.Yes)
             {
+                //after deleteting replace the element with -
                 myArray[currentIndex, 0] = "-";
                 myArray[currentIndex, 1] = "-";
                 myArray[currentIndex, 2] = "-";
@@ -376,22 +398,28 @@ namespace IntraWiki
 
         #region SORT AND SEARCH
 
+     //   8.4	Write the code for a Bubble Sort method to sort the 2D array by Name ascending.
+     //   Ensure you use a separate swap method that passes(by reference) the array element to be swapped(do not use any built-in array methods),
         private void sortByColumn(int sortedColumn)
         {
+            //parametised with sortedColumn so that we can use the method to sort by any column when required.
             int num = sortedColumn;
             for (int i = 0; i < rowSize -1; i++)
             {
                 for (int rows = 0; rows < rowSize -i - 1; rows++)
                 {
+                    // getting null or empty value means there is no further data, so break from loop.
                     if (string.IsNullOrEmpty(myArray[rows, num]) || string.IsNullOrEmpty(myArray[rows + 1, num]))
                     {
                         break;
                     }
+                    // to sort - (logic that if other element is - then dont excute the below line and go to another rows)
                     if (myArray[rows + 1, num].Equals("-"))
                     {
                         continue;
                     }
 
+                    // now after above - we continue and now [row,num] is - and another element is not - then do swap. this will take - to the end of the array.
                     if ((myArray[rows, num].CompareTo(myArray[rows + 1, num]) > 0) || myArray[rows, num].Equals("-") && !myArray[rows + 1, num].Equals("-"))
                     {
                         swap(num, rows, myArray);
@@ -400,13 +428,16 @@ namespace IntraWiki
             }
 
         }
+
         private void swap(int columnToBeSorted, int rows, string[,] twoDArray)
         {
             string tempForColumn = twoDArray[rows, columnToBeSorted];
             twoDArray[rows, columnToBeSorted] = twoDArray[rows + 1, columnToBeSorted];
             twoDArray[rows + 1, columnToBeSorted] = tempForColumn;
+            //this will sort the other rows according to the sorted column
             for (int column = 0; column < colSize; column++)
             {
+               // skip the sorted column that we choosed to be sorted
                 if(column == columnToBeSorted)
                 {
                     continue;
@@ -417,6 +448,7 @@ namespace IntraWiki
             }
         }
 
+        //label created so that we can click on the label to sort the listbox
         private void labelName_Click(object sender, EventArgs e)
         {
             sortByColumn(0);
@@ -431,11 +463,14 @@ namespace IntraWiki
             toolStripStatusLabel1.Text = "Data Sorted By Category...";
         }
 
+       // 8.5	Write the code for a Binary Search for the Name in the 2D array and display the information in the other textboxes when found
+       // add suitable feedback if the search in not successful and clear the search textbox(do not use any built-in array methods),
         private void buttonSearch_Click(object sender, EventArgs e)
         {
             sortByColumn(0);
             DisplayArray();
             string target = textBoxSearch.Text.ToUpper();
+            //calling binarySearch method which return tuple with the value of mid and found.
             var (mid, found) = binarySearch(target);
 
             if (found)
@@ -455,6 +490,7 @@ namespace IntraWiki
 
         }
 
+        //method returns tuple which contains the value of mid and found
         private (int, bool) binarySearch(string target)
         {
             int min = 0;
@@ -483,6 +519,7 @@ namespace IntraWiki
         #endregion
 
         #region UTILITIES
+        //when the textbox is changed in the textbox then clear the error, this is implemented for Add function
         private void textBoxName_TextChanged(object sender, EventArgs e)
         {
             errorProvider1.Clear();
@@ -503,11 +540,13 @@ namespace IntraWiki
             errorProvider1.Clear();
         }
 
+        // to clear the prvious user feedback.
         private void IntraWiki_MouseDown(object sender, MouseEventArgs e)
         {
             toolStripStatusLabel1.Text = "!Application is Running...";
         }
 
+        //8.3	Create a CLEAR method to clear the four text boxes so a new definition can be added,
         private void clearTextBox()
         {
             textBoxName.Clear();
